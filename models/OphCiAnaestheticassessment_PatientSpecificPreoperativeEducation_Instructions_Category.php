7665565751a3db5e8607f98465fa3502aa2dafa8
Richard Willis <willis.rh@gmail.com>
2014-08-08 16:10:02 +0100

    [ORB-344] Patient instructions and categories schema and admin. Removed diabetes instructions.

---
+++ models/OphCiAnaestheticassessment_PatientSpecificPreoperativeEducation_Instructions_Category.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * This is the model class for table "ophcianassessment_speced_instruc_cat".
+ *
+ * The followings are the available columns in table 'ophcianassessment_speced_instruc_cat':
+ * @property string $id
+ * @property string $name
+ * @property integer $display_order
+ * @property string $last_modified_user_id
+ * @property string $last_modified_date
+ * @property string $created_user_id
+ * @property string $created_date
+ *
+ * The followings are the available model relations:
+ * @property User $user
+ * @property User $usermodified
+ */
+class OphCiAnaestheticassessment_PatientSpecificPreoperativeEducation_Instructions_Category extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'ophcianassessment_speced_instruc_cat';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		return array(
+			array('name', 'safe'),
+			array('name', 'required'),
+			array('id, name', 'safe', 'on' => 'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+			'user' => array(self::BELONGS_TO, 'User', 'created_user_id'),
+			'usermodified' => array(self::BELONGS_TO, 'User', 'last_modified_user_id'),
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'ID',
+			'name' => 'Name',
+			'display_order' => 'Display Order'
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id,true);
+		$criteria->compare('name',$this->name,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return OphCiAnaestheticassessment_PatientSpecificPreoperativeEducation_Instructions_Category the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+}
---
+++ views/admin/patient_instruction_categories_row.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * OpenEyes
+ *
+ * (C) Moorfields Eye Hospital NHS Foundation Trust, 2008-2011
+ * (C) OpenEyes Foundation, 2011-2013
+ * This file is part of OpenEyes.
+ * OpenEyes is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * OpenEyes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with OpenEyes in a file titled COPYING. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package OpenEyes
+ * @link http://www.openeyes.org.uk
+ * @author OpenEyes <info@openeyes.org.uk>
+ * @copyright Copyright (c) 2008-2011, Moorfields Eye Hospital NHS Foundation Trust
+ * @copyright Copyright (c) 2011-2013, OpenEyes Foundation
+ * @license http://www.gnu.org/licenses/gpl-3.0.html The GNU General Public License V3.0
+ */
+?>
+
+<tr id="cat-<?php echo $row->id;?>">
+	<td class="reorder">
+		<span>&uarr;&darr;</span>
+	</td>
+	<td>
+		<?php echo CHtml::hiddenField('id[]',$row->id)?>
+		<?php
+		$htmlOptions = array();
+		if (!$row->active) {
+			$htmlOptions['disabled'] = 'disabled';
+			echo CHtml::hiddenField('name[]',$row->name);
+		}
+		echo CHtml::textField('name[]',$row->name,$htmlOptions);
+		if (isset($errors[$i])) {?>
+			<span class="error">
+				<?php echo $errors[$i]?>
+			</span>
+		<?php }?>
+	</td>
+	<td>
+		<?php echo CHtml::checkBox('active[' . $row->id . ']',$row->active);?>
+	</td>
+	<td>
+		<a href="/OphCiAnaestheticassessment/admin/editPatientInstructions?id=<?php echo $row->id;?>">manage instructions</a>
+	</td>
+</tr>
